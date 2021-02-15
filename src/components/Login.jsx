71d3361e9025dffa2f7e0d800c34fe85e66ad88d
sonu-kumar2001 <sonurocckkerr@gmail.com>
2021-02-16 02:42:01 +0530

    emplemented login and signup functionalities

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,72 @@
 import React, { Component } from 'react'
-import { NavLink } from "react-router-dom";
+import { NavLink,Redirect } from "react-router-dom";
 import Header from './Header';
 
 export default class Register extends Component {
     
+    constructor(props) {
+        super()
+        this.state = {
+            email: "",
+            password: "",
+            isUserLoggedIn : false,
+            errors: {
+                email: "",
+                password: "",
+            }
+        }
+    }
+    validateEmail = (email) => {
+		const regex = /\S+@\S+\.\S+/;
+		return regex.test(email);
+    }
+    validatePassword = (password)=> {
+        const regex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+        return regex.test(password);
+    }
+
+    changeHandler = ({target}) => {
+        let {name,value} = target;
+        let error = this.state.errors;
+
+        switch (name) {
+            case "email" :
+                error.email = this.validateEmail(value) ? "" : "Email is not valid"
+                break;
+            case "password" :
+                error.password = this.validatePassword(value)? "" : "Passwors should contain atleast a number and specail character"
+                break;
+            default:
+                break;
+        }
+        this.setState({
+            error,[name]: value
+        })
+    }
+
+    submitHandler = async (event) => {
+        event.preventDefault();
+        const user = {
+            "user" : {
+                "email": this.state.email,
+                "password":this.state.password
+            }
+        }
+        try {         
+            await fetch(`/api/users/login`,{method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify(user)})
+               .then((res) => res.json())
+               .then((data) => localStorage.setItem('token',data.user.createdToken))
+               .then(() => this.setState({isUserLoggedIn:!this.state.isUserLoggedIn}))               
+          } catch (error) {
+              console.error('Error',error)
+          }
+    }
+
     render() {
+        const{email,password} = this.state.errors;
+        if(localStorage.getItem('token') || this.state.isUserSignup){
+            return   <Redirect to='/' />
+        }
         return (
             <section className="flex">
                 <Header/>
@@ -14,10 +76,10 @@ export default class Register extends Component {
                         <div className="form-image">
                             <img src="/media/form.jpg" alt="form-svg"/>
                         </div>
-                    <form>
+                    <form onSubmit={this.submitHandler}>
                         <div className="field">
                             <p className="control has-icons-left has-icons-right">
-                                <input className="input" type="email" placeholder="Email" />
+                                <input className={email ? "error input" : "success input"} type="email" placeholder="Email" name="email" value={this.state.email} onChange={this.changeHandler} />
                                 <span className="icon is-small is-left">
                                 <i className="fas fa-envelope"></i>
                                 </span>
@@ -26,9 +88,10 @@ export default class Register extends Component {
                                 </span>
                             </p>
                         </div>
+                        <span className="text-red">{email}</span>
                         <div className="field">
                             <p className="control has-icons-left has-icons-right">
-                                <input className="input" type="password" placeholder="Password" />
+                            <input className={password ? "error input" : "success input"} type="password" placeholder="Password" name="password" value={this.state.password} onChange={this.changeHandler} />
                                 <span className="icon is-small is-left">
                                 <i class="fas fa-lock"></i>
                                 </span>
@@ -37,6 +100,7 @@ export default class Register extends Component {
                                 </span>
                             </p>
                         </div>
+                        <span className="text-red">{password}</span>
                         <div className="field">
                             <p className="control">
                                 <button type="submit" className="button is-success">Login</button>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Link } from "react-router-dom";
+import { Link,Redirect } from "react-router-dom";
 import Header from "./Header"
 
 export default class Register extends Component {
@@ -10,6 +10,7 @@ export default class Register extends Component {
             email: "",
             password: "",
             confirmPassword: "",
+            isUserSignup : false,
             errors: {
                 username: "",
                 email: "",
@@ -39,7 +40,7 @@ export default class Register extends Component {
                 error.email = this.validateEmail(value) ? "" : "Email is not valid"
                 break;
             case "password" :
-                error.password = this.validatePassword(value)? "Passwors should contain atleast a number and specail character" : ""
+                error.password = this.validatePassword(value)? "" : "Passwors should contain atleast a number and specail character"
                 break;
             case "confirmPassword" :
                 error.confirmPassword = this.state.password === value ? "" : "Password didn't matched"
@@ -52,8 +53,31 @@ export default class Register extends Component {
         })
     }
 
+    submitHandler = async (event) => {
+        event.preventDefault();
+        const user = {
+            "user" : {
+                "username":this.state.username,
+                "email": this.state.email,
+                "password":this.state.password
+            }
+        }
+        try {         
+            await fetch(`/api/users`,{method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify(user)})
+               .then((res) => res.json())
+               .then((data) => localStorage.setItem('token',data.user.createdToken))
+               .then(() => this.setState({isUserSignup:!this.state.isUserSignup}))               
+          } catch (error) {
+              console.error('Error',error)
+          }
+    }
+
     render() {
         const{username,email,password,confirmPassword} = this.state.errors;
+
+        if(localStorage.getItem('token') || this.state.isUserSignup){
+            return   <Redirect to='/' />
+        }
         return (
             <section className="flex">
                 <Header/>
@@ -63,7 +87,7 @@ export default class Register extends Component {
                         <div className="form-image">
                             <img src="/media/form.jpg" alt="form-svg"/>
                         </div>
-                    <form>
+                    <form onSubmit= {this.submitHandler}>
                         <div className="field">
                             <p className="control has-icons-left has-icons-right">
                                 <input className={username ? "error input" : "success input"}  type="text" placeholder="Username" name="username" value={this.state.username} onChange={this.changeHandler} />
